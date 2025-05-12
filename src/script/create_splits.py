d360eddcc428d7a2807e7c064508707003a5a950
alfoCaiazza <acaiazza2000@gmail.com>
2025-05-12 17:50:46 +0200

    Splitting the dataset into train, val and test partition

---
+++ src/script/create_splits.py
@@ -0,0 +1,33 @@
+import os
+import random
+import shutil
+from sklearn.model_selection import train_test_split
+
+dataset_dir = 'src/data/LIS-fingerspelling-dataset'
+output_dir = 'src/data/processed'
+
+classes = os.listdir(dataset_dir)
+os.makedirs(output_dir, exist_ok=True)
+
+def copy_files(file_list, split, output_dir, cls):
+    split_dir = os.path.join(output_dir, split, cls)
+    os.makedirs(split_dir, exist_ok=True)
+    for file in file_list:
+        shutil.copy(file, split_dir)
+
+for cls in classes:
+    cls_dir = os.path.join(dataset_dir, cls)
+    images = [os.path.join(cls_dir, img) for img in os.listdir(cls_dir)]
+
+    # Images shuffling to prevent data unbalancing
+    random.shuffle(images)
+
+    # Train/Test split
+    train_imgs, temp_imgs = train_test_split(images, test_size=0.3, random_state=42)
+    val_imgs, test_imgs = train_test_split(temp_imgs, test_size=0.5, random_state=42)
+
+    copy_files(train_imgs, "train", output_dir, cls)
+    copy_files(val_imgs, "val", output_dir, cls)
+    copy_files(test_imgs, "test", output_dir, cls)
+
+
